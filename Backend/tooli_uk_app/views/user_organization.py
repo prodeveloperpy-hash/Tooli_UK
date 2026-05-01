ea3bd1567a634c7283af85351e41947b2ecd216d
Fiaz <prodeveloper.py@gmail.com>
2026-05-01 09:09:22 +0500

    tini

--- Backend/tooli_uk_app/views/user_organization.py
+++ Backend/tooli_uk_app/views/user_organization.py
@@ -4,7 +4,6 @@ from tooli_uk_app.filters.user_organization import UserOrganizationFilter
 from tooli_uk_app.models import UserOrganization
 from tooli_uk_app.serializers.user_organization import UserOrganizationSerializer
 
-
 class UserOrganizationViewSet(viewsets.ModelViewSet):
     queryset = UserOrganization.objects.all()
     serializer_class = UserOrganizationSerializer
