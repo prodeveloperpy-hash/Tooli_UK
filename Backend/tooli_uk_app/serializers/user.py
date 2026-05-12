e7be5dfe3d2fb3459394dbd4fa358c6e86c67e7e
Fiaz <prodeveloper.py@gmail.com>
2026-05-12 12:44:07 +0500

    hasformate

--- Backend/tooli_uk_app/serializers/user.py
+++ Backend/tooli_uk_app/serializers/user.py
@@ -1,4 +1,5 @@
 from django.urls import reverse
+from django.contrib.auth.hashers import make_password
 from rest_framework import serializers
 
 from tooli_uk_app.models import User
@@ -25,3 +26,20 @@ class UserSerializer(serializers.ModelSerializer):
         path = reverse("user-avatar", kwargs={"pk": instance.pk})
         data["avatar_url"] = request.build_absolute_uri(path)
         return data
+
+    def _hash_password_if_present(self, validated_data: dict) -> dict:
+        password = validated_data.get("password")
+        if password:
+            validated_data["password"] = make_password(password)
+        elif "password" in validated_data:
+            # Do not overwrite an existing password with empty value.
+            validated_data.pop("password")
+        return validated_data
+
+    def create(self, validated_data):
+        validated_data = self._hash_password_if_present(validated_data)
+        return super().create(validated_data)
+
+    def update(self, instance, validated_data):
+        validated_data = self._hash_password_if_present(validated_data)
+        return super().update(instance, validated_data)
